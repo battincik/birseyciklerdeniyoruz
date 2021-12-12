c69c63f7004b9b1df3f97d8027aa22922f7b4a43
Bahattin <51765819+battincik@users.noreply.github.com>
2021-12-13 00:24:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const moment = require('moment');
 
 for(let i=0; i<10;i++){
-  console.log('EDIT 1234567 ZA sj EHE 31 31 YALIN HARİKA EZK ZAAAAA 31 AHAHAHA 31')
+  console.log('EDIT 1234567 ZA sj EHE 31 31 YALIN HARİKA EZK ZAAAAA 31 AHAHAHA 31 31')
   console.log(moment().format('LLL'))
 }
